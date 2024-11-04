09a115740afcf3e62741b68d689542080b52b5f2
Ada-Bardho <ada.bardho@kapsys.at>
2024-11-04 11:23:39 +0100

    Made about section not repetable

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -549,7 +549,7 @@ export interface ApiHomepageHomepage extends Struct.SingleTypeSchema {
     >;
     seo: Schema.Attribute.Component<'shared.seo', false>;
     heroSection: Schema.Attribute.Component<'section.hero-section', false>;
-    aboutSection: Schema.Attribute.Component<'section.about-section', true>;
+    aboutSection: Schema.Attribute.Component<'section.about-section', false>;
     statisticsSection: Schema.Attribute.Component<
       'section.statistics-section',
       false
