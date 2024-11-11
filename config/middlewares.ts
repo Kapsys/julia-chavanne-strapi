8856ab88da0935aaf88b0689e686fcdb9c60e7af
Volodymyr <peganovvova@gmail.com>
2024-11-11 10:47:23 +0200

    Add ckeditor

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,6 +8,7 @@ export default [
                 useDefaults: true,
                 directives: {
                     'connect-src': ["'self'", 'https:'],
+                    'script-src': ['https://cdn.ckeditor.com'],
                     'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 's3.eu-central-1.amazonaws.com'],
                     'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 's3.eu-central-1.amazonaws.com'],
                     upgradeInsecureRequests: null,
